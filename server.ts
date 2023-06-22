74664b3edeb1590996031a772df6e61b215785f3
shub766 <singhshubham6324@gmail.com>
2023-06-22 19:18:07 +0530

    scaling application with cluster

--- server.ts
+++ server.ts
@@ -2,16 +2,37 @@ import express, { Request, Response } from 'express';
 import mongoose, { Schema, Document } from 'mongoose';
 import {Cinema} from './model/cinemaSchema'
 import {cinemaRouter} from './routes/cinemaRouter';
+import cluster from 'cluster';
+import os from 'os';
+
 mongoose.connect('mongodb://localhost:27017/cinema');
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 const app = express();
 app.use(express.json());
 
 //Routing
 app.use('/cinemas', cinemaRouter);
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+if(cluster.isMaster){
+  const workers = os.cpus().length;
+  for(let i=0;i<workers;i++){
+    cluster.fork();
+  }
+
+  cluster.on('online',(worker)=> {
+    console.log(`worker started with a PID of:${worker.process.pid}`);
+  });
+
+  cluster.on('exit',(worker,code,signal) => {
+    console.log(`worker died with a PID of:${worker.process.pid}`);
+    cluster.fork();
+
+  });
+} else {
+  app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
+}
